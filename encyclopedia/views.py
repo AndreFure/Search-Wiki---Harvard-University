f860a19371ede37fb4173690f6b4cd8a5d292768
Andres Genes <andresgenes@hotmail.com>
2021-08-10 20:54:01 -0300

    Segundo commit

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 from . import util
 
+entries = {
+    "css": "My css",
+    "Django": "My Django",
+    "HTML": "My HTML",
+    "Python": "My Python"
+}
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
