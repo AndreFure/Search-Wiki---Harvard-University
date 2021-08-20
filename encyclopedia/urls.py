7e9839048c56b6389c076c6d71855031e7e3b107
Andres Genes <andresgenes@hotmail.com>
2021-08-20 16:25:25 -0300

    Tercer commit

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("create", views.create, name="create"),
+    path("wiki/<str:entry>/edit", views.editEntry, name="editEntry"),
+    path("wiki/<str:title>/submit", views.submitEditEntry, name="submitEditEntry"),
+    path("search", views.search, name="search"),
+    path("wiki/", views.randomEntry, name="randomEntry")
 ]
