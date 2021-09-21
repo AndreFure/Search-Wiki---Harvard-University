a8abbe3c9bfb4d0c6aa3fefb31104ed82f104260
Andres Genes <andresgenes@hotmail.com>
2021-09-21 20:03:44 -0300

    Second commit

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,10 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry"),
-    path("create", views.create, name="create"),
-    path("wiki/<str:entry>/edit", views.editEntry, name="editEntry"),
-    path("wiki/<str:title>/submit", views.submitEditEntry, name="submitEditEntry"),
-    path("search", views.search, name="search"),
-    path("wiki/", views.randomEntry, name="randomEntry")
+    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("newEntry", views.newEntry, name="newEntry"),
+    path("wiki/<str:entry>/edit", views.edit, name="edit"),
+    path("random", views.random, name="random"),
+    path("search", views.search, name="search")
 ]
